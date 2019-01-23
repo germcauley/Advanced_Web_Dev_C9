e9116f2eaa1a7c337c9d2e0f6589599d2e75381c
germcauley <geraldmcauley@gmail.com>
2019-01-23 23:21:11 +0000

    add a route

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,15 +1,19 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000
     
+var  todoRoutes = require('./routes/todos')  
 
 app.get('/', function(req,res){
-    res.send("HI THERE FROM EXPRESS!!!!")
+    res.send("HELLO FROM THE ROOT ROUTE")
 });  
-    
+
+
+app.use('/api/todos', todoRoutes);
     
 
 app.listen(process.env.PORT, function(){
-    console.log("APP IS RUNNING ON PORT "+ process.env.PORT);
+    console.log("APP IS RUNNING ON PORT "+ port);
 })
     
 
