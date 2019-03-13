3d5d5103fc9f42d3b8d5797df0e5dc92c903a697
germcauley <geraldmcauley@gmail.com>
2019-03-13 23:19:36 +0000

    Added html css and js files for todos app

--- todos_api/index.js
+++ todos_api/index.js
@@ -4,14 +4,15 @@ var express = require('express'),
     bodyParser = require('body-parser');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-   
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/views'));
     
 var  todoRoutes = require('./routes/todos')  
 
 
 
 app.get('/', function(req,res){
-    res.send("HELLO FROM THE ROOT ROUTE")
+    res.send("index.html");
 });  
 
 
