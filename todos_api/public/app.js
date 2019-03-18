af5dc38ea1d5db516be2e06f849bdba108cc8f0b
germcauley <geraldmcauley@gmail.com>
2019-03-18 21:46:23 +0000

    get Json ajax call

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -1,1 +1,16 @@
-alert('HI FROM APP.JS');
+/* global $ */
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(function(data){
+        console.log(data);
+    })
+});
+
+
+function addTodos(todos){
+    todos.forEach(function(todo){
+        var newTodo = $('<li class ="task">'+todo.name + '</li>');
+        $('.list').append(newTodo);
+       console.log(todo.name); 
+    });
+}
